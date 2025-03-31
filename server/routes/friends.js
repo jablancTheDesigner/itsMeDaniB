5dfa2ab8c567e785e431150f5bc81d3b00553d3f
Jahmal Blanchard <jablancdesigns@gmail.com>
2025-03-31 16:56:09 -0500

    Vercel confg no mongo

--- server/routes/friends.js
+++ server/routes/friends.js
@@ -11,34 +11,47 @@ import { ObjectId } from "mongodb";
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();
 
+const friends = [
+  {
+    id: 1,
+    firstName: "Test",
+    lastName: "Test",
+    email: "Test@email.com",
+  }
+]
+
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("users");
-  let results = await collection.find({}).toArray();
-  res.send(results).status(200);
+  // let collection = await db.collection("users");
+  // let results = await collection.find({}).toArray();
+  // res.send(results).status(200);
+  res.send(friends).status(200);
+  
 });
 
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("users");
-  let query = { _id: new ObjectId(req.params.id) };
-  let result = await collection.findOne(query);
+  // let collection = await db.collection("users");
+  // let query = { _id: new ObjectId(req.params.id) };
+  // let result = await collection.findOne(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+  // if (!result) res.send("Not found").status(404);
+  // else res.send(result).status(200);
+  res.send(friends).status(200);
 });
 
 // This section will help you create a new record.
 router.post("/", async (req, res) => {
   try {
-    let newDocument = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-    };
-    let collection = await db.collection("users");
-    let result = await collection.insertOne(newDocument);
-    res.send(result).status(204);
+    // let newDocument = {
+    //   firstName: req.body.firstName,
+    //   lastName: req.body.lastName,
+    //   email: req.body.email,
+    // };
+    // let collection = await db.collection("users");
+    // let result = await collection.insertOne(newDocument);
+    // res.send(result).status(204);
+    res.send(friends).status(200);
   } catch (err) {
     console.error(err);
     res.status(500).send("Error adding record");
@@ -48,18 +61,19 @@ router.post("/", async (req, res) => {
 // This section will help you update a record by id.
 router.patch("/:id", async (req, res) => {
   try {
-    const query = { _id: new ObjectId(req.params.id) };
-    const updates = {
-      $set: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-      },
-    };
+    // const query = { _id: new ObjectId(req.params.id) };
+    // const updates = {
+    //   $set: {
+    //     firstName: req.body.firstName,
+    //     lastName: req.body.lastName,
+    //     email: req.body.email,
+    //   },
+    // };
 
-    let collection = await db.collection("users");
-    let result = await collection.updateOne(query, updates);
-    res.send(result).status(200);
+    // let collection = await db.collection("users");
+    // let result = await collection.updateOne(query, updates);
+    // res.send(result).status(200);
+    res.send(friends).status(200);
   } catch (err) {
     console.error(err);
     res.status(500).send("Error updating users");
@@ -69,12 +83,13 @@ router.patch("/:id", async (req, res) => {
 // This section will help you delete a record
 router.delete("/:id", async (req, res) => {
   try {
-    const query = { _id: new ObjectId(req.params.id) };
+    // const query = { _id: new ObjectId(req.params.id) };
 
-    const collection = db.collection("users");
-    let result = await collection.deleteOne(query);
+    // const collection = db.collection("users");
+    // let result = await collection.deleteOne(query);
 
-    res.send(result).status(200);
+    // res.send(result).status(200);
+    res.send(friends).status(200);
   } catch (err) {
     console.error(err);
     res.status(500).send("Error deleting record");
