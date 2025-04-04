a20e68c909939a1d1fc16fe21512fdf8cdcd935c
Jahmal Blanchard <jablancdesigns@gmail.com>
2025-04-04 01:22:17 -0500

    Stlye frontend v1

--- client/src/components/brandingContent.jsx
+++ client/src/components/brandingContent.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const BrandingContent = () => {
     return (
-        <div className="container">
+        <div className="container py-6 px-4 text-center mx-auto">
             {/* logo */}
-            <h1>itsDaniB</h1>
+            <h1 className="font-bold">itsDaniB</h1>
         </div>
     )
 }
--- client/src/components/formContent.jsx
+++ client/src/components/formContent.jsx
@@ -8,13 +8,17 @@ const FormContent = () => {
         email: ""
     });
 
+    const [successMsg, setSuccessMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     // This function will handle the submission.
     const onSubmit = async (e) => {
         e.preventDefault();
         const friend = { ...formData };
+        setLoading(true)
         try {
         // if the id is present, we will set the URL to /record/:id, otherwise we will set the URL to /record.
-        const response = await fetch(`http://localhost:5050/api/v1`, {
+        const response = await fetch(`https://its-me-dani-b-server.vercel.app/api/v1/`, {
             // if the id is present, we will use the PATCH method, otherwise we will use the POST method.
             method: `POST`,
             headers: {
@@ -27,7 +31,12 @@ const FormContent = () => {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        console.log(`response: `, response)
+        const jsonData = await response.json();
+
+        setSuccessMsg(jsonData.message)
+        setLoading(false)
+
+        console.log(`response: `, jsonData)
 
         } catch (error) {
             console.error('A problem occurred with your fetch operation: ', error);
@@ -42,37 +51,60 @@ const FormContent = () => {
 
     return (
         <>
-            {/* form */}
-            <h3>Stay Connected</h3>
-            {/* heading */}
-            <p>Get added to our mailing list to stay updated on all the cool stuff our creative family has to offer.</p>
-            <form onSubmit={onSubmit}>
-               <div className="mb-2">
-                    <label>First Name</label>
-                    <input name="firstName" value={formData.firstName} onChange={(e) => setFormData({
-                        ...formData,
-                        firstName: e.target.value
-                    })} required />
-               </div>
-
-                <div className="mb-2">
-                    <label>Last Name</label>
-                    <input name="lastName" value={formData.lastName} onChange={(e) => setFormData({
-                        ...formData,
-                        lastName: e.target.value
-                    })} required />
+            <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:text-gray-800 my-auto">
+                <div className="flex flex-col justify-between">
+                    <div className="my-auto">
+                        <h2 className="text-4xl font-bold leading-tight lg:text-5xl text-fuchsia-800">Stay Connected!</h2>
+                        <div className="dark:text-gray-600 text-3xl">Get added to our mailing list to stay updated on all the cool stuff our creative family has to offer.</div>
+                    </div>
                 </div>
+                <form onSubmit={onSubmit} className="relative">
+                    <div className="mb-2">
+                            <label className="text-sm text-fuchsia-800 font-bold">First Name</label>
+                            <input className="w-full p-3 rounded bg-white" name="firstName" value={formData.firstName} onChange={(e) => setFormData({
+                                ...formData,
+                                firstName: e.target.value
+                            })} required />
+                    </div>
 
-                <div className="mb-2">
-                    <label>Email</label>
-                    <input name="email" type="email" value={formData.email} onChange={(e) => setFormData({
-                        ...formData,
-                        email: e.target.value
-                    })} required />
-                </div>
+                    <div className="mb-2">
+                        <label className="text-sm text-fuchsia-800 font-bold">Last Name</label>
+                        <input className="w-full p-3 rounded bg-white" name="lastName" value={formData.lastName} onChange={(e) => setFormData({
+                            ...formData,
+                            lastName: e.target.value
+                        })} required />
+                    </div>
 
-                <button type="submit">Submit</button>
-            </form>
+                    <div className="mb-2">
+                        <label className="text-sm text-fuchsia-800 font-bold ">Email</label>
+                        <input className="w-full p-3 rounded bg-white" name="email" type="email" value={formData.email} onChange={(e) => setFormData({
+                            ...formData,
+                            email: e.target.value
+                        })} required />
+                    </div>
+
+                    <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded  bg-fuchsia-800 text-gray-50 cursor-pointer ">Submit</button>
+                    {loading && (
+                        <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full flex items-center justify-center bg-fuchsia-800">
+                            <div className="flex items-center justify-center space-x-2">
+                                <div className="w-4 h-4 rounded-full animate-pulse bg-white"></div>
+                                <div className="w-4 h-4 rounded-full animate-pulse bg-white"></div>
+                                <div className="w-4 h-4 rounded-full animate-pulse bg-white"></div>
+                            </div>
+                        </div>
+                    )}
+                    {successMsg && (
+                        <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full flex items-center justify-center">
+                            <div className="flex shadow-md gap-6 rounded-lg overflow-hidden max-w-[300px] bg-fuchsia-800 text-gray-800 divide-fuchsia-950">
+                                <div className="flex flex-1 flex-col p-4 border-l-8 dark:border-violet-600">
+                                    <span className="text-2xl text-gray-50 font-bold leading-tight">Thank you!</span>
+                                </div>
+                                <button className="px-4 flex items-center text-xs uppercase tracking-wide text-gray-50 border-l border-l-fuchsia-950 cursor-pointer bg-fuchsia-950" onClick={() => setSuccessMsg(null)}>Dismiss</button>
+                            </div>
+                        </div>
+                    )}
+                </form>
+            </div>
         </>
     )
 }
