61c7fabd9c5ab1f3b3541db5d5380a408bcd1752
Jahmal Blanchard <jablancdesigns@gmail.com>
2025-04-04 01:35:09 -0500

    Minor footer update

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
             <BrandingContent />
             <FormContent />
             {/* footer */}
-            <div className="dark:bg-gray-100 dark:text-gray-900">
+            <div>
                 <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row dark:divide-gray-600">
                     <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
                         <li>About</li>
