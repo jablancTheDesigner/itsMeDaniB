8b400e413dbff5ab1207aa64b4925995cfc8cfdc
Jahmal Blanchard <jablancdesigns@gmail.com>
2025-04-04 02:32:40 -0500

    Header padding bottom removal

--- client/src/components/brandingContent.jsx
+++ client/src/components/brandingContent.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/SVG/logo.svg"
 
 const BrandingContent = () => {
     return (
-        <div className="container py-16 px-4 text-center mx-auto flex justify-center fill-amber-600">
+        <div className="container pt-16 px-4 text-center mx-auto flex justify-center fill-amber-600">
             {/* logo */}
             <img className="w-16 h-auto" src={Logo}/> 
         </div>
